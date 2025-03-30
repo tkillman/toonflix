9745a7955a8d868f734416715f4fbbfb3e48830e
kimdonggyu <dkkim1004@hanmail.net>
2025-03-30 22:48:49 +0900

    pomo 앱

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,14 +18,122 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+int defaultTime = 10;
+
+class _HomePageState extends State<HomePage> {
+  Timer? _timer;
+  int time = defaultTime;
+  bool isStart = false;
+  int pomoCnt = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        isStart = true;
+        time--;
+        if (time == 0) {
+          pomoCnt++;
+          time = defaultTime;
+          isStart = false;
+          _timer?.cancel();
+        }
+      });
+    });
+  }
+
+  void stopTimer() {
+    _timer?.cancel();
+    setState(() {
+      isStart = false;
+    });
+  }
+
+  String formatTime(int time) {
+    int min = time ~/ 60;
+    int sec = time % 60;
+    return '${min.toString().padLeft(2, '0')}:${sec.toString().padLeft(2, '0')}';
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello, World!'),
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.red[400],
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Center(
+                  child: Text(
+                    formatTime(time),
+                    style: const TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Center(
+                  child: IconButton(
+                    icon: isStart
+                        ? const Icon(Icons.pause_circle_outline_rounded)
+                        : const Icon(Icons.play_arrow),
+                    iconSize: 150,
+                    onPressed: isStart ? stopTimer : startTimer,
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.blue[400],
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Pomodoro',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            '$pomoCnt',
+                            style: const TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
